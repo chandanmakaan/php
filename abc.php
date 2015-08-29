7d4be7d3a6df9c81da3fd4c74a9bdfbcd8d6676f
chandan kumar <chandan.k@makaan.com>
2015-08-29 11:40:46 +0530

    test

--- abc.php
+++ abc.php
@@ -1,3 +1,10 @@
-<?php 
+<script>
+var templatePath = 'test.marko';
+var re = new RegExp("/^([a-z]+.marko)/");
+if (re.test(templatePath)) {
+	console.log("Valid");
+} else {
+	console.log("Invalid");
+}
 
-echo phpinfo(); 
+</script>
